d3bc3f1e8e690390420110c8995e8f674ab7b835
lucasElPapiDeTuMami <lucassantanapr@gmail.com>
2022-05-04 12:58:58 -0500

    lol

--- test_camera.py
+++ test_camera.py
@@ -4,9 +4,16 @@ from CarSupport import Camera
 c = Camera()
 
 print("hi")
+MAXSIZE = 100
+psi = [0] * MAXSIZE
 for i in range(100):
    img = c.take_picture()
-   psi = c.compute_psi(img,DEBUG=True)
+   p = c.compute_psi(img,DEBUG=True)
    time.sleep(0.1)
    print(i)
+   psi[i] = p
 
+with open('PSI.txt','w') as f:
+    for i in range(MAXSIZE):
+        f.write(f"{psi[i]}")
+        f.write('\n')
